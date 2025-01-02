4a9f220a6ac33f83b45a3e4b098823d729859c97
bourbonut <ben.bourbon06@gmail.com>
2025-01-02 21:50:09 +0100

    Replace `p1`, `p2` and `p3` with `Vec` in `Builder`

--- src/builders.rs
+++ src/builders.rs
@@ -5,25 +5,25 @@ use egui_plot;
 use crate::shapes::{Circle, Line, Draw, Shape};
 
 
-fn build_line(points: [&Option<PlotPoint>; 2]) -> Option<Box<dyn Shape>> {
+fn build_line(points: [Option<&PlotPoint>; 2]) -> Option<Box<dyn Shape>> {
     let [p1, p2] = points;
     if p1.is_none() || p2.is_none() {
         return None;
     } 
     let p1 = p1.unwrap();
     let p2 = p2.unwrap();
-    Some(Box::new(Line::new([p1, p2])))
+    Some(Box::new(Line::new([*p1, *p2])))
 }
 
 
-fn partial_draw_line(p: &Option<PlotPoint>, plot_ui: &mut PlotUi, current_point: PlotPoint) {
+fn partial_draw_line(p: Option<&PlotPoint>, plot_ui: &mut PlotUi, current_point: PlotPoint) {
     if let Some(p1) = p {
         let line = Line::new([*p1, current_point]);
         line.draw(plot_ui);
     }
 }
 
-fn build_circle(points: [&Option<PlotPoint>; 3]) -> Option<Box<dyn Shape>> {
+fn build_circle(points: [Option<&PlotPoint>; 3]) -> Option<Box<dyn Shape>> {
     let [p1, p2, p3] = points;
     if p1.is_none() || p2.is_none() || p3.is_none() {
         return None;
@@ -31,15 +31,14 @@ fn build_circle(points: [&Option<PlotPoint>; 3]) -> Option<Box<dyn Shape>> {
     let p1 = p1.unwrap();
     let p2 = p2.unwrap();
     let p3 = p3.unwrap();
-    Some(Box::new(Circle::new([p1, p2, p3])))
+    Some(Box::new(Circle::new([*p1, *p2, *p3])))
 }
 
 
-fn partial_draw_circle(points: [&Option<PlotPoint>; 2], plot_ui: &mut PlotUi, current_point: PlotPoint) {
+fn partial_draw_circle(points: [Option<&PlotPoint>; 2], plot_ui: &mut PlotUi, current_point: PlotPoint) {
     if let Some(p1) = points[0] {
         if let Some(p2) = points[1] {
             let circle = Circle::new([*p1, *p2, current_point]);
-            //println!("Partial circle: {:?}", circle);
             circle.draw(plot_ui);
         } else {
             plot_ui.points(
@@ -68,26 +67,20 @@ pub enum BuilderMode {
 }
 
 pub struct Builder {
-    p1: Option<PlotPoint>,
-    p2: Option<PlotPoint>,
-    p3: Option<PlotPoint>,
+    points: Vec<PlotPoint>,
     mode: BuilderMode,
 }
 
 impl Builder {
     pub fn new() -> Self {
         Self {
-            p1: None,
-            p2: None,
-            p3: None,
+            points: Vec::new(),
             mode: BuilderMode::Line,
         }
     }
 
     pub fn reset(&mut self) {
-        self.p1 = None;
-        self.p2 = None;
-        self.p3 = None;
+        self.points.clear();
     }
 
     pub fn set_mode(&mut self, mode: BuilderMode) {
@@ -98,27 +91,22 @@ impl Builder {
     }
 
     pub fn set_next_point(&mut self, point: PlotPoint) {
-        if self.p1.is_none() {
-            self.p1 = Some(point);
-            return;
-        } else if self.p2.is_none() {
-            self.p2 = Some(point);
-            return;
+        if self.points.len() < 3 {
+            self.points.push(point);
         }
-        self.p3 = Some(point);
     }
 
     pub fn draw(&self, plot_ui: &mut PlotUi, current_point: PlotPoint) {
         match self.mode {
-            BuilderMode::Line => partial_draw_line(&self.p1, plot_ui, current_point),
-            BuilderMode::Circle => partial_draw_circle([&self.p1, &self.p2], plot_ui, current_point),
+            BuilderMode::Line => partial_draw_line(self.points.get(0), plot_ui, current_point),
+            BuilderMode::Circle => partial_draw_circle([self.points.get(0), self.points.get(1)], plot_ui, current_point),
         }
     }
 
     pub fn build(&self) -> Option<Box<dyn Shape>> {
         match self.mode {
-            BuilderMode::Line => build_line([&self.p1, &self.p2]),
-            BuilderMode::Circle => build_circle([&self.p1, &self.p2, &self.p3]),
+            BuilderMode::Line => build_line([self.points.get(0), self.points.get(1)]),
+            BuilderMode::Circle => build_circle([self.points.get(0), self.points.get(1), self.points.get(2)]),
         }
     }
 }
